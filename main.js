db0c197a5a189cfcaa0aa3c6bb4762a1806c640a
Vyas gv <73235709+Vyas120@users.noreply.github.com>
2021-01-14 11:36:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+    song = loadSound("Beleiver(fairlane_remix).mp3");
+    song2 = loadSound("centuries.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function play(){
+    song.play();
+}
